8834b10fa7d61ce27d030a9c96ceb17677e4e55a
André Pinto <estadoalpha@gmail.com>
2012-12-16 22:51:41 +0000

    adding default gigs configuration file

---
+++ protected/config/Default_GigsConfig.php
@@ -0,0 +1,21 @@
+<?php
+
+class GigsConfig
+{
+
+    const DB_NAME       = '';
+    const DB_USERNAME   = '';
+    const DB_PASSWORD   = '';
+
+    const GII_PASSWORD  = '';
+
+    const SOUNDCLOUD_APP_OAUTH_CLIENT_ID        = '';
+    const SOUNDCLOUD_APP_OAUTH_CLIENT_SECRET    = '';
+
+    const TWITTER_APP_OAUTH_CUSTOMER_KEY        = '';
+    const TWITTER_APP_OAUTH_CUSTOMER_SECRET     = '';
+
+    const FACEBOOK_APP_ID       = '';
+    const FACEBOOK_APP_SECRET   = '';
+
+}
